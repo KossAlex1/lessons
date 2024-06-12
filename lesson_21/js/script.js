675f7e07deb9091fd1804a86ab25b7a4dbb271fd
KossAlex1 <163010427+KossAlex1@users.noreply.github.com>
2024-06-12 23:38:28 +0300

    Add files via upload

---
+++ lesson_21/js/script.js
@@ -0,0 +1,23 @@
+// Строгий режим
+"use strict"
+let numOne = 20
+let numTwo = "20"
+
+let numSumm = numOne + numTwo
+console.log(numSumm)
+
+// const userName = 20
+// userName = "Іштван"
+
+
+// let userAge = 30
+// let userName = `Саня`
+// // let userInfo = "Ім'я: " + userName + ", вік: " + userAge
+// let userInfo = `Ім'я: ${userName}, вік: ${userAge}`
+// console.log(userInfo);
+
+
+
+
+
+
